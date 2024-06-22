a69698427f6c7677f8b5925545397d66010145aa
Diego Lamarão <diego.lamarao92@gmail.com>
2024-06-21 23:06:29 -0300

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/js-music-notes/'
 })
